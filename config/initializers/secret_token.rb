aff10ed7d2a0ab16af0b47694f611ea8b0f4a6e7
3milylin <3milylin@gmail.com>
2013-05-10 01:02:16 -0400

    base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WahnLive::Application.config.secret_token = '739837553bf9676dc196b8a3b706955ab8e656c0c43e7480de7cbbab355a3ae312d6457a1d679f25d6371e459c44de9a10c172c98793d656d9545832fd414ebc'
