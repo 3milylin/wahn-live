9b420cc691666d90585269f1a5960b535d3d30c3
3milylin <3milylin@gmail.com>
2013-05-17 12:41:17 -0400

    tweaks

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -3,5 +3,6 @@ require 'csv'
 class MapController < ApplicationController
   def index	
   	@venues = Venue.find(:all, :conditions => ['herenow > ?', 0])
+  	@peopleCount = Venue.sum("herenow")
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,26 @@
 require 'csv'
 
 # populate NYC points 
-	CSV.foreach(Rails.public_path + "/data/nyc.csv", :headers => true) do |row|
-		Point.create(
-			:lat => row.to_s.split(/[\n ,]+/).first, 
-			:lng => row.to_s.split(/[\n ,]+/).last
-			)
-	end
+	#CSV.foreach(Rails.public_path + "/data/nyc.csv", :headers => true) do |row|
+	#	Point.create(
+	#		:lat => row.to_s.split(/[\n ,]+/).first, 
+	#		:lng => row.to_s.split(/[\n ,]+/).last
+	#		)
+	#end
 	
 # populate test venues 
 	CSV.foreach(Rails.public_path + "/data/test-data.csv", :headers => true) do |row|
 		temp = row.to_s.split(',')
-		if !Venue.find_by_foursqr_id(temp[0])
-			Venue.create(
-				:foursqr_id => temp[0],
-				:name => temp[1].tr(?", ?'),
-				:lat => temp[2],
-				:lng => temp[3],
-				:herenow => temp[4],
-				:category => temp[5]
-				)
+		if temp[4].to_i > -1
+			if !Venue.find_by_foursqr_id(temp[0]) && 
+				Venue.create(
+					:foursqr_id => temp[0],
+					:name => temp[1].tr(?", ?'),
+					:lat => temp[2],
+					:lng => temp[3],
+					:herenow => temp[4],
+					:category => temp[5]
+					)
+			end
 		end 
 	end
