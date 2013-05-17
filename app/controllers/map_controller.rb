46da0c2f40bba4ebf91aa8c08269153048924b57
3milylin <3milylin@gmail.com>
2013-05-17 13:11:40 -0400

    fixing update time

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,5 +4,6 @@ class MapController < ApplicationController
   def index	
   	@venues = Venue.find(:all, :conditions => ['herenow > ?', 0])
   	@peopleCount = Venue.sum("herenow")
+  	@lastUpdated = Venue.maximum("updated_at")
   end
 end
