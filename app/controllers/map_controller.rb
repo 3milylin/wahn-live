e77623c57f88f0eabe2349efb89be691d61cc72e
3milylin <3milylin@gmail.com>
2013-05-20 13:04:51 -0400

    tweak

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,5 +5,6 @@ class MapController < ApplicationController
   	@venues = Venue.find(:all, :conditions => ['herenow > ?', 0])
   	@peopleCount = Venue.sum("herenow")
   	@lastUpdated = Venue.maximum("updated_at")
+  	
   end
 end
